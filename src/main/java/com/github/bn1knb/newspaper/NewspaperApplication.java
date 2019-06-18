3fc8eda78e30d0c76c6eaf28e07c312614ae704d
Anton <tonykomik@gmail.com>
2019-06-18 14:45:47 +0300

    removed duplicate entrypoint

--- src/main/java/com/github/bn1knb/newspaper/NewspaperApplication.java
+++ src/main/java/com/github/bn1knb/newspaper/NewspaperApplication.java
@@ -1,4 +1,4 @@
-package com.github.bn1knb.newspaper;
+package com.github.newspaper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
