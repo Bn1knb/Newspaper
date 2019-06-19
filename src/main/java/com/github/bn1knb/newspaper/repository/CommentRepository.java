382341d3c0c46daf7e408861a691626a97348e92
Anton <tonykomik@gmail.com>
2019-06-19 12:34:13 +0300

    deleted duplicate files

--- src/main/java/com/github/bn1knb/newspaper/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.newspaper.repository;
-
-import com.github.newspaper.entity.Comment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-}
--- src/main/java/com/github/bn1knb/newspaper/repository/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.newspaper.repository;
-
-import com.github.newspaper.entity.News;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NewsRepository extends CrudRepository<News, Long> {
-}
