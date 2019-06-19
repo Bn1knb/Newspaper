c51bb481e2f9648971cff4b452ff176eb5d54707
Anton <37040301+Bn1knb@users.noreply.github.com>
2019-06-19 15:01:07 +0300

    Revert "Develop"

--- src/main/java/com/github/newspaper/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.newspaper.repository;
-
-import com.github.newspaper.entity.Comment;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-}
