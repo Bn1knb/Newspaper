0a234ccca35427602b4d4b1622b8cb9ce8081f86
Illia Mikhalkou <illia.mikhalkou@itechart-group.com>
2019-07-01 14:15:35 +0300

    fixed images

---
+++ src/main/java/com/github/newspaper/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.github.newspaper.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+}
